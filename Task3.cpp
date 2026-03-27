faad60bb4896fc7eb61c3cf07e776879aa0b7ad5
KapibaraGosling <137946228+KapibaraGosling@users.noreply.github.com>
2026-03-27 14:37:05 +0300

    Create Task3.cpp
    Ivanov Kirill TKI 242

---
+++ Task3.cpp
@@ -0,0 +1,33 @@
+/*Даны векторы V1 и V2 с различным количеством элементов. 
+Найти все числа (с учетом повторений), которые входят в один из исходных 
+векторов и отсутствуют в другом, и вывести их в порядке убывания; 
+при этом если, например, некоторое число входит в один из векторов 3 раза, 
+а в другой 5 раз, то его надо вывести 2 раза. 
+Использовать алгоритм set_symmetric_difference для двух вспомогательных мультимножеств 
+и итератора ptout_iterator. Чтобы обеспечить вывод чисел в нужном порядке, 
+при создании множеств и в алгоритме использовать функциональный объект greater.  */
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <iterator>
+#include <algorithm>
+
+
+
+int main(){
+    std::vector<int> vec1 = {10, 20, 30, 40, 50, 60};
+	std::vector<int> vec2 = {60, 60, 20, 35, 40, 100, 15};
+
+	std::multiset <int, std::greater<int>> mulset1(vec1.begin(), vec1.end());
+	std::multiset <int, std::greater<int>> mulset2(vec2.begin(), vec2.end());
+
+	std::set_symmetric_difference(
+		mulset1.begin(), mulset1.end(),
+		mulset2.begin(), mulset2.end(),
+		std::ostream_iterator<int>(std::cout, " "),
+		std::greater<int>());
+	std::cout << std::endl;
+	return 0;
+
+}
