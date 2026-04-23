823838690e62e4fa95b3c02e2883536d19a1fa62
KapibaraGosling <137946228+KapibaraGosling@users.noreply.github.com>
2026-04-23 16:19:36 +0300

    Update Task4.cpp

--- Task4.cpp
+++ Task4.cpp
@@ -8,28 +8,20 @@
 #include <algorithm>
 #include <functional>
 
-std::vector<int> input_vector(){
-	std::vector<int> vec;
-	int val;
-	while (std::cin >> val){
-		vec.push_back(val);
-	}
-	std::cin.clear();
-	return vec;
-}
-
 
 int main(){
 	std::cout<<"Введите элементы вектора через пробел\n";
-    std::vector<int> V = input_vector();
+    std::vector<int> V{std::istream_iterator<int>(std::cin), 
+                   std::istream_iterator<int>()};
+    std::cin.clear();
 	if(V.empty()){
 		throw std::invalid_argument("Ошибка: пустой вектор.");
 	}
 	int K;
 	std::cout<<"Введите число K\n";
 	std::cin >> K;
-	auto substract = std::bind(std::minus<int>(), std::placeholders::_1,K);
-	std::transform(V.begin(),V.end(), V.begin(), substract);
+	auto subtract = std::bind(std::minus<int>(), std::placeholders::_1,K);
+	std::transform(V.begin(),V.end(), V.begin(), subtract);
 	std::cout<<"Итоговый результат:\n";
 
 	for(auto x:V){
